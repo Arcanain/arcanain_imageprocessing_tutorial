cef51bc4c84b2649e8d1b4b8d2f5b58239c8b52a
nkn4ryu <shiryu.nakano@gmail.com>
2024-09-16 21:03:39 +0900

    mentainance

--- src/image_sub_view.cpp
+++ src/image_sub_view.cpp
@@ -3,15 +3,25 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <string>
 
 class MinimalImageSubscriber : public rclcpp::Node
 {
 public:
-  MinimalImageSubscriber() : Node("opencv_image_subscriber")
+  MinimalImageSubscriber(const std::string& topic_name, int width, int height) 
+  : Node("opencv_image_subscriber"), target_width_(width), target_height_(height)
   {
-    // Initialize subscriber
+    // Initialize subscriber with the given topic name
     subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "/image_raw_transformed", 10, std::bind(&MinimalImageSubscriber::image_callback, this, std::placeholders::_1));
+      topic_name, 2, std::bind(&MinimalImageSubscriber::image_callback, this, std::placeholders::_1));
+
+    RCLCPP_INFO(this->get_logger(), "Subscribed to topic: %s", topic_name.c_str());
+  }
+
+  ~MinimalImageSubscriber()
+  {
+    // Close all OpenCV windows when the node is being destroyed
+    cv::destroyAllWindows();
   }
 
 private:
@@ -20,6 +30,11 @@ private:
     try {
       // Convert ROS image message to OpenCV image
       cv::Mat cv_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
+
+      // Resize the image if target dimensions are specified
+      if (target_width_ > 0 && target_height_ > 0) {
+        cv::resize(cv_image, cv_image, cv::Size(target_width_, target_height_));
+      }
 
       // Display image
       cv::imshow("Subscribed Image", cv_image);
@@ -30,12 +45,38 @@ private:
   }
 
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+  int target_width_;
+  int target_height_;
 };
 
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<MinimalImageSubscriber>();
+
+  // Default values
+  std::string topic_name = "/image_raw";
+  int width = 0;  // Default: don't resize
+  int height = 0; // Default: don't resize
+
+  // Check if a topic name is provided via command line
+  if (argc > 1) {
+    topic_name = argv[1];
+  }
+
+  // Check if width and height are provided via command line
+  if (argc > 3) {
+    width = std::stoi(argv[2]);
+    height = std::stoi(argv[3]);
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Starting node with topic: %s", topic_name.c_str());
+  if (width > 0 && height > 0) {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Resizing image to: %d x %d", width, height);
+  }
+
+  // Create the subscriber node with the specific topic name and image size
+  auto node = std::make_shared<MinimalImageSubscriber>(topic_name, width, height);
+
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
